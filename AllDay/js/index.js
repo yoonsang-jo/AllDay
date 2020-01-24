7a1370d1ca86009eb2d410075a3889faeda2f7f8
yoonsang-jo <whdbstkd000@gmail.com>
2020-01-24 22:27:03 +0900

    AllDay-version-3

--- AllDay/js/index.js
+++ AllDay/js/index.js
@@ -3,14 +3,26 @@ $(function () {
     var subHeight = $(window).height() - $(".btn").height();
     //alert(subHeight);
     $(".sub-wrap").css('min-height', subHeight);
+    $(".sub-wrap").css('padding-bottom', $(".btn").height());
     $(window).resize(function(){
         var subHeight = $(window).height() - $(".btn").height();
         $(".sub-wrap").css('min-height', subHeight);
+        $(".sub-wrap").css('padding-bottom', $(".btn").height());
     });
     /* //SUB-WRAP LAYOUT */
     
+    
+    /* 인증하기 버튼 */
+    $(".send").click(function(){
+        $(this).text("재발송");
+        $(this).removeClass("blue");
+        $(".authenticate").css('display','block');
+    });
+    /* //인증하기 버튼 */
+    
+    
     /* 인풋 텍스트 클리어 */
-    $(".input-outer input").keyup(function(){
+    $(".input-outer > input").keyup(function(){
         var inputLen = $(this).val().length; 
         //alert(inputLen);
         if(inputLen > 0){
@@ -22,9 +34,11 @@ $(function () {
     
     $(".input-clear").click(function () {
         $(this).prev().val('');
+        $(this).prev().focus();
         $(this).hide();
     });
     /* //인풋 텍스트 클리어 */
+    
     
     /* 뒤로가기 */
     // 사파리에서 동작 안함 (추후 수정)
@@ -32,4 +46,9 @@ $(function () {
         history.back();
     });
     /* //뒤로가기 */
+    
+    
+    /* 팝업 */
+    
+    /* //팝업 */
 });
